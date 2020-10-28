527159aa7b9014dd071352ab3309675403476e9a
atsteinberg <alexts404dev@gmail.com>
2020-10-28 16:53:42 +0100

    initial commit

---
+++ templates/component.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MYCOMPONENT.module.scss';
+
+function MYCOMPONENT (props) {
+  return (
+    <div className={styles.MYCOMPONENT}>
+    MYCOMPONENT works!
+    </div>
+  );
+}
+
+export default MYCOMPONENT;
